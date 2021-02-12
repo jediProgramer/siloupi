8b4838336c407351c0165ef96ec5434e014a19c4
priyayidimas <priyayidimas@upi.edu>
2021-02-13 00:06:20 +0700

    cek cmd

--- application/modules/student/controllers/Student.php
+++ application/modules/student/controllers/Student.php
@@ -211,21 +211,21 @@ class Student extends CI_Controller {
 		$cmd = "java -jar Debug.jar 7";
 
 		// CARA 1 (SERVER ONLY)
-			// $descriptorspec = array(
-			//    0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
-			//    1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
-			//    2 => array("pipe", "w")    // stderr is a pipe that the child will write to
-			// );
-			// flush();
-			// $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
-			// echo "<pre>";
-			// if (is_resource($process)) {
-			// 	while ($s = fgets($pipes[1])) {
-			// 		print $s;
-			// 		flush();
-			// 	}
-			// }
-			// echo "</pre>";
+			$descriptorspec = array(
+			   0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+			   1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+			   2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+			);
+			flush();
+			$process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+			echo "<pre>";
+			if (is_resource($process)) {
+				while ($s = fgets($pipes[1])) {
+					print $s;
+					flush();
+				}
+			}
+			echo "</pre>";
 		
 		// CARA 2
 			// while (@ ob_end_flush()); // end all output buffers if any
@@ -239,12 +239,13 @@ class Student extends CI_Controller {
 			// }
 			// echo '</pre>';
 		// CARA 3 (SERVER ONLY)
-			$result = $this->shell->liveExecuteCommand($cmd);
-			if($result['exit_status'] === 0){
-				echo '<br><p id="success">Program Berhasil</p>';
-			 } else {
-				echo '<br><p id="success">Program Tidak Berhasil</p>';
-			 }
+			// $result = $this->shell->liveExecuteCommand($cmd);
+			// if($result['exit_status'] === 0){
+			//  } else {
+			// 	echo '<br><p id="success">Program Tidak Berhasil</p>';
+			//  }
+		echo '<br><p id="success">Program Berhasil</p>';
+
 	}
 
 	function phpini(){

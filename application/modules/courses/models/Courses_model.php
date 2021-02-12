4a7eb9be3a97ebad548f81936cfe11c9a7bb5b95
priyayidimas <priyayidimas@upi.edu>
2021-02-12 23:40:14 +0700

    import

--- application/modules/courses/models/Courses_model.php
+++ application/modules/courses/models/Courses_model.php
@@ -50,7 +50,7 @@
 
 		public function createOrUpdate($data){
 			$sql = 'INSERT INTO siloupi_courses(idcourses, idcurriculum, idcoursescategory, idcoursessubcategory, idsemester, courses, credit, idprograme) '.
-			'VALUES (?, ?, ?, ?, ?, ?, ?) '.
+			'VALUES (?, ?, ?, ?, ?, ?, ?, ?) '.
 			'ON CONFLICT(idcourses, idcurriculum, idprograme) DO UPDATE SET '.
 			'idcoursescategory=EXCLUDED.idcoursescategory, idcoursessubcategory=EXCLUDED.idcoursessubcategory, idsemester=EXCLUDED.idsemester, courses=EXCLUDED.courses, credit=EXCLUDED.credit';
 			$this->db->query($sql, $data);
